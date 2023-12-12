1ada688d30a55c587db7dcacd7c69ba36fe6024e
pakmarina108@gmail.com <pm@mail.ru>
2023-12-12 16:30:10 +0900

    updated valid api key

--- src/main/java/com/chatgptspring/controller/GptController.java
+++ src/main/java/com/chatgptspring/controller/GptController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
 @RestController
 public class GptController {
     @Qualifier("gpt")
@@ -24,17 +26,15 @@ public class GptController {
 
     @GetMapping("/ask")
     public String chat(@RequestParam String prompt) {
-        // create a request
         GptRequest request = new GptRequest(model, prompt);
 
-        // call the API
         GptReply response = restTemplate.postForObject(apiUrl, request, GptReply.class);
 
         if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
             return "No response";
         }
 
-        // return the first response
+        // return response
         return response.getChoices().get(0).getMessage().getContent();
     }
 }
